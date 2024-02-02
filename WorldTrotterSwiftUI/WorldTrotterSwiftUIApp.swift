8167a5a162bc6a30ae9b27d738487fef87c513fe
Preet Karia <karia.preet5@gmail.com>
2024-02-01 20:47:01 -0500

    Initial Commit

---
+++ WorldTrotterSwiftUI/WorldTrotterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTrotterSwiftUIApp.swift
+//  WorldTrotterSwiftUI
+//
+//  Created by Preet Karia on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WorldTrotterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
